1c3090183ce4b444bd10891f677c4b6e7aa89bdc
Matthew Ranford <matt.ranford16@gmail.com>
2023-11-08 16:17:32 +1300

    Button added to refresh data displaying on page

--- client/components/Randomevent.tsx
+++ client/components/Randomevent.tsx
@@ -1,28 +1,35 @@
 import { useState, useEffect } from 'react'
-
 import { getRandomEvent } from '../apiClient.ts'
-
 import type { RandomEvent } from '../../models/RandomEvent.ts'
 
 export default function Events() {
   const [randomEvent, setRandomEvent] = useState<RandomEvent | null>()
 
   useEffect(() => {
-    async function fetchRandomEvent() {
+    fetchRandomEvent()
+  }, [])
+
+  const fetchRandomEvent = async () => {
+    try {
       const eventData = await getRandomEvent()
       setRandomEvent(eventData)
-    }
-    try {
-      fetchRandomEvent()
     } catch (error) {
       console.log(error)
     }
-  }, [])
+  }
+
+  const handleClick = () => {
+    fetchRandomEvent()
+  }
+
   return (
     <>
       <div className="main-container">
         <h1 className="event-header">Random Events</h1>
         <pre className="data-text">{JSON.stringify(randomEvent, null, 2)}</pre>
+        <button onClick={handleClick} className="refresh" type="button">
+          Find a new event
+        </button>
       </div>
     </>
   )
