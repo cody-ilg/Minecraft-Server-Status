32d8c0e66dc492c97d9b316e87a27a5d354d4b34
student <student@pop-os.localdomain>
2023-11-08 14:01:39 +1300

    nearly got ditto image

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -8,3 +8,9 @@ export function getWelcome(): Promise<Welcome> {
   return request.get(`${serverURL}/welcome`).then((response) => response.body)
 }
 // ***   ***   ***
+
+export async function getPokemon(){
+  const response = await request.get(`https://pokeapi.co/api/v2/pokemon/ditto`)
+  console.log(response.body)
+  return response.body
+}
---
+++ client/components/Pokemon.tsx
@@ -0,0 +1,31 @@
+// What's your starter?
+import { useEffect, useState } from 'react'
+
+import { getPokemon } from '../apiClient'
+import type { Pokemon } from '../../models/Pokemon.ts'
+
+export default function Pokemon() {
+  const [pokemon, setPokemon] = useState<Pokemon | null>()
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const pokemonData = await getPokemon()
+      setPokemon(pokemonData)
+    }
+
+    try {
+      fetchPokemon()
+    } catch (error) {
+      console.log(error)
+    }
+  },[])
+
+  return (
+    <>
+      <h2>Pokémon </h2>
+      {/* {<pre>{JSON.stringify(pokemon, null, 2)}</pre>} */}
+      {pokemon?.name}<br/><br/>
+      <img src={pokemon?.sprites.front_shiny} alt={pokemon?.name} />
+    </>
+  )
+}
