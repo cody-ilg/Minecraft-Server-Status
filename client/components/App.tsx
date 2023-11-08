35312fe699d70dd41ea3350e4f51f47fcf606d0a
Keeley Owen <keeleytowen@gmail.com>
2023-11-08 22:25:57 +1300

    fixing pokemon errors

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,21 +1,30 @@
 import { useState, useEffect } from 'react'
-
+import { Pokemon } from './Pokemon.tsx'
 import { getWelcome } from '../apiClient.ts'
+import { LOTR } from './LOTR.tsx'
+
+// function App() {
+//   const [welcomeStatement, setWelcomeStatement] = useState('')
+
+//   useEffect(() => {
+//     getWelcome()
+//       .then((res) => {
+//         setWelcomeStatement(res.statement)
+//       })
+//       .catch((err) => {
+//         console.error(err.message)
+//       })
+//   })
+
+//   return <h1>{welcomeStatement}</h1>
+// }
 
 function App() {
-  const [welcomeStatement, setWelcomeStatement] = useState('')
-
-  useEffect(() => {
-    getWelcome()
-      .then((res) => {
-        setWelcomeStatement(res.statement)
-      })
-      .catch((err) => {
-        console.error(err.message)
-      })
-  })
-
-  return <h1>{welcomeStatement}</h1>
+  return (
+    <>
+      <Pokemon />
+    </>
+  )
 }
 
 export default App
