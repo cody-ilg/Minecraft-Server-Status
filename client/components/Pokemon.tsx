8a03d927f5f2acd19f16189e220ef77d2d0546e5
Keeley Owen <keeleytowen@gmail.com>
2023-11-08 22:32:28 +1300

    still broke :((

--- client/components/Pokemon.tsx
+++ client/components/Pokemon.tsx
@@ -1,9 +1,27 @@
 import { useState, useEffect } from 'react'
-
 import { getSalamence } from '../apiClients/pokemon.ts'
 import type { Pokemon } from '../../models/Pokemon.ts'
 
 export function Pokemon() {
-  const [pokemon, setPokemon] = useState<Pokemon>
+  const [pokemon, setPokemon] = useState<Pokemon | null>(null)
 
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const salamenceData = await getSalamence()
+        setPokemon(salamenceData)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchPokemon()
+  }, [])
+
+  return (
+    <>
+      <h2>SALAMENCE</h2>
+      <pre>{JSON.stringify(pokemon, null)}</pre>
+    </>
+  )
 }
