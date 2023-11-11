7a8d7191da482272795f2387816c01cd6cb7c7a0
williford abunas <abunaswilliford.com>
2023-11-11 21:50:03 +1300

    no significant changes

--- client/components/RandomActivity.tsx
+++ client/components/RandomActivity.tsx
@@ -17,7 +17,7 @@ export default function RandomActivity() {
   //     setRActivity(rActivityData)
   //   }
   //   fetchRandomActivity()
-  // }, [setRActivity])
+  // }, [setRActivity])(())
 
   return (
     <>
